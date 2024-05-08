c6aebf97af1ca437e72b5829f443d9527daa1d77
jigth <danielochoamontes@gmail.com>
2024-05-08 00:13:16 -0500

    FEAT: Finish basic products CRUD (standalone)
    For now this CRUD doesn't work with any other business domain, just with itself so is very simple but good enough for a Frontend CRUD and basic CI/CD

--- backend/src/modules/products/infrastructure/controllers/products.ts
+++ backend/src/modules/products/infrastructure/controllers/products.ts
@@ -22,6 +22,8 @@ export class ProductsController {
         this.router.get('/', this.getAllProducts)
         this.router.get(`/:id`, this.getProductById)
         this.router.post(`/`, this.createProduct)
+        this.router.put(`/:id`, this.updateProduct)
+        this.router.delete(`/:id`, this.deleteProduct)
     }
 
     public getAllProducts = async (req: Request, res: Response) => {
@@ -71,6 +73,60 @@ export class ProductsController {
 
             const newProduct = await this.productsSvc.createProduct(product)
             res.status(200).send({ id: newProduct.id });
+        } catch (err) {
+            console.log(err)
+            res.status(500).send({ error: "Error while getting the product" })
+        }
+    }
+
+    private updateProduct = async (req: Request, res: Response) => {
+        try {
+            const id = req.params.id;
+
+            const {
+                cost,
+                name,
+                imageUrl,
+                inventory,
+                description
+            } = req.body;
+
+            const productToUpdate: Product = {
+                id,
+                cost,
+                name,
+                imageUrl,
+                inventory,
+                description
+            }
+
+            console.log({productToUpdate})
+
+            const updatedProduct = await this.productsSvc.updateProduct(productToUpdate)
+            console.log({updatedProduct})
+            if (!updatedProduct) {
+                res.status(404).send({ error: "Product to update not found" })
+                return
+            }
+
+            res.status(200).send({ updatedProduct });
+        } catch (err) {
+            console.log(err)
+            res.status(500).send({ error: "Error while updating the product" })
+        }
+    }
+
+    private deleteProduct = async (req: Request, res: Response) => {
+        try {
+            const id = req.params.id;
+            const product = await this.productsSvc.deleteProduct(id);
+
+            if (!product) {
+                res.status(404).send({ error: "Product not found" })
+                return
+            }
+
+            res.status(200).send({ product });
         } catch (err) {
             console.log(err)
             res.status(500).send({ error: "Error while getting the product" })
